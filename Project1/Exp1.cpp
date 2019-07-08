6ee44dc51198c8ef15d85f82425b2bbc124feaff
Raymond <raymonddaniel.ronquillo.eng@ust.edu.ph>
2019-07-09 01:44:28 +0800

    RealProblem1-1

--- Project1/Exp1.cpp
+++ Project1/Exp1.cpp
@@ -1,31 +1,61 @@
-#include <iostream> 
-#include <conio.h> 
+#include <iostream>
+#include <conio.h>
+#include <cmath>
 
 using namespace std;
+int main()
 
-int Density(int a, int b) 
+{
 
-{ 
-	int r; 
-	r = (a/(4*b)); 
-	return r; 
-}
 
-int main() 
+	// prices of tickets
+	int Price_P, Price_S, Price_B, Price_GA;
 
-{ 
-	int x, y{}, z;
-	cout << "Volume Calculator" << endl;
-	cout << "Input mass in grams: "; cin >> x; 
-	cout << x << "g";
-	cout << "Input density in grams/(m^3)"; cin >> y;
-	cout << y << "g/(m^3)"<<endl;
-	
-z = Density(x, y); 
+	Price_P = 250, Price_S = 100, Price_B = 50, Price_GA = 25;
 
-cout << "The volume is " << z; 
+	// quantity of tickets sold
+	int Sold_P, Sold_S, Sold_B, Sold_GA;
+	int Total_Tickets_Sold, Total_Tickets_Sales;
 
-_getch();
+	// FOR PREMIUM
+	cout << "Enter the number of Premium tickets sold:";
+	cin >> Sold_P;
+	cout << "The total sales for premium tickets is:" << (Sold_P * Price_P) << endl;
 
-return 0; 
+	cout << endl;
+
+	// FOR SIDELINE
+	cout << "Enter the number of Sideline tickets sold:";
+	cin >> Sold_S;
+	cout << "The total sales for Sideline tickets is:" << (Sold_S * Price_S) << endl;
+
+	cout << endl;
+
+	// FOR BOX
+	cout << "Enter the number of Box tickets sold:";
+	cin >> Sold_B;
+	cout << "The total sales for Box tickets is:" << (Sold_B * Price_B) << endl;
+
+	cout << endl;
+
+	// FOR GENERAL ADMISSION
+	cout << "Enter the number of General admission tickets sold:";
+	cin >> Sold_GA;
+	cout << "The total sales for General amission tickets is:" << (Sold_GA * Price_GA) << endl;
+
+	cout << endl;
+
+	//NUMBER TICKETS SOLD
+	Total_Tickets_Sold = Sold_P + Sold_S + Sold_B + Sold_GA;
+
+	cout << "The total tickets sold is:" << Total_Tickets_Sold;
+
+	cout << endl;
+
+	//TOTAL TICKETS SALES
+	Total_Tickets_Sales = Sold_GA * Price_GA + Sold_GA * Price_GA + Sold_S * Price_S + Sold_P * Price_P;
+	cout << "The total ticket sales is:" << Total_Tickets_Sales;
+
+	_getch();
+	return 0;
 }
